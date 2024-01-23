9b7adbc5085f79632c7061fcb85d9e5a655d3db4
Jaehyo <troup@naver.com>
2024-01-24 08:46:08 +0900

    Feat: 프로젝트 생성 (#1)

---
+++ apiGateway/src/test/java/com/snaprhythm/gateway/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snaprhythm.gateway.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
