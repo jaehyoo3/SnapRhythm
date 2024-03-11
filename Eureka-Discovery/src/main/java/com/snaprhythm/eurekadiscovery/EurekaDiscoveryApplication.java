ea64c8e2fca78d9e30a4da24627b119b31f3b570
Jaehyo <troup@naver.com>
2024-03-11 15:22:07 +0900

    feat: GATEWAY 셋팅 및 EUREKA 셋팅 VideoSerivce S3 사용 준비 MSA 구조 변경 -> FEED, LIKEM COMMENT -> VIDEO 통합 CHAT서비스 추가(#6)

--- Eureka-Discovery/src/main/java/com/snaprhythm/eurekadiscovery/EurekaDiscoveryApplication.java
+++ Eureka-Discovery/src/main/java/com/snaprhythm/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.snaprhythm.eurekadiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryApplication {
 
     public static void main(String[] args) {
